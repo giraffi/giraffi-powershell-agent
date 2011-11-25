fafe90f8e49c026a267f8aabeb1ac8c050793474
sawanoboly <sawanoboriyu@higanworks.com>
2011-11-25 16:44:57 +0900

    add task script du=1h -> du=23:59 and starttime 00:00

--- install.bat
+++ install.bat
@@ -30,8 +30,10 @@ rem ## add task.
 set TaskName=\Giraffi\Giraffi-Powershell-Agent
 set Task=powershell %AgentDirPath%agent.ps1
 set Schedule=DAILY
+set ST=00:00
 set RI=5
+set DU=23:59
 rem /F=Force
-schtasks /create /tn "%TaskName%" /tr "%Task%" /sc %Schedule% /ri %RI% /ru "" /F
+schtasks /create /tn "%TaskName%" /tr "%Task%" /sc %Schedule% /ri %RI% /st %ST% /du %DU% /ru "" /F
 
 endlocal
