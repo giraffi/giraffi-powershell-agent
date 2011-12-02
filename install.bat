64eb598e34ea8446646a01cd0d9c6ddedf30cd9c
sawanoboly <sawanoboriyu@higanworks.com>
2011-12-02 15:44:44 +0900

    add banner.txt and some messages for installer.

--- install.bat
+++ install.bat
@@ -13,6 +13,7 @@ rem ## Change directory to script dir.
 set CuurentDirPath=%~dp0
 cd /d "%CuurentDirPath%"
 
+type banner.txt
 
 set AgentDirPath=c:\opt\agent\
 
@@ -25,11 +26,16 @@ IF NOT EXIST %AgentDirPath% mkdir %AgentDirPath%
 
 rem # Copy or build script.
 rem powershell build_agent.ps1
+echo.
+echo Install to %AgentDirPath%
 copy .\agent.ps1 %AgentDirPath% /Y
 
 rem ## put apikey.
-set /P APIKEY="Please paste GIRAFFI_APIKEY here > "
+echo.
+echo Please paste GIRAFFI_APIKEY here. (or edit %AgentDirPath%apikey.txt later.)
+set /P APIKEY="> "
 echo %APIKEY%>%AgentDirPath%apikey.txt
+echo.
 
 rem ## add task.
 set TaskName=\Giraffi\Giraffi-Powershell-Agent
@@ -41,4 +47,11 @@ set DU=23:59
 rem /F=Force
 schtasks /create /tn "%TaskName%" /tr "%Task%" /sc %Schedule% /ri %RI% /st %ST% /du %DU% /ru "" /F
 
+echo.
+echo ############################
+echo Thank you for using girafi.
+echo   Agent install finished.
+echo ############################
+echo.
+pause
 endlocal
